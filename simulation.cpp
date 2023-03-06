25420738230c5d5cb18e345a630cb7b157c74d99
Anusha <anushasharma824@gmail.com>
2023-03-06 23:40:34 +0530

    added request class

--- simulation.cpp
+++ simulation.cpp
@@ -1,7 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Request {
+    string name;
+    string person;
+    string time;
+    string remaining_time;
+
+    public:
+        Request(string name, string person, string time) {
+            this->name = name;
+            this->person = person;
+            this->time = time;
+            this->remaining_time = time;
+        }
+};
+
+class RoundRobin {
+    private:
+        int time_slice;
+        queue<Request> qS;
+        queue<Request> qT;
+        // current time
+        int cur;
+
+    public:
+        RoundRobin(int time_slice, string requests[][5]) {
+            this->time_slice = time_slice;
+            // this->requests = requests
+        }
+
+};
+
 int main() {
+
+    // Enter time slice
+    int tq;
+    cout << "Enter time for each appointment: ";
+    cin >> tq;
 
     // Ask for number of requests from user
     int requests;
@@ -43,8 +79,15 @@ int main() {
         }
         while(data[i][4] != "s" && data[i][4] != "t" && data[i][4] != "S" && data[i][4] != "T");
 
+        transform(data[i][4].begin(), data[i][4].end(), data[i][4].begin(), ::tolower);
+
+        Request(data[i][1], data[i][4], data[i][3]);
+
         cout << '\n';
     }
+
+    // Create scheduler object
+    RoundRobin scheduler(tq, data);
 
     // Display data
     cout << "Process ID: " << "      " << "Name: " << "      " << "Arrival Time" << "      " << "Burst Time: " << "      " << "Student/Teacher" << '\n';
